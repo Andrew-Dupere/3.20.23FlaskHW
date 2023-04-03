e56b9e8bb4a8055116cfb522bd79521e105e7913
andrewdupere <andrewdupere@outlook.com>
2023-04-03 19:43:58 -0400

    added 5 favs

--- app/routes.py
+++ app/routes.py
@@ -3,12 +3,15 @@ from flask import render_template
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+
+    listofthings = ['Sushi','Steak','Ice Cream','Fruit','Cheese']
+
+    return render_template('index.html', listofthings = listofthings)
 
 
 @app.route('/page2')
 def pagetwo():
-
+    listofthings = ['Sushi','Steak','Ice Cream','Fruit','Cheese']
     
-    return render_template('page2.html')
+    return render_template('page2.html', things = listofthings, name = 'Andrew')
 
